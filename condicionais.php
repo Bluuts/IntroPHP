e862d70f1aac74d1960cf84e96b1607693f434c2
Bluuts <gh21032001@gmail.com>
2023-06-14 11:38:43 -0300

    Brincadeira tem hora
    Primeiro Teste

---
+++ condicionais.php
@@ -0,0 +1,41 @@
+<?php
+    #CONDICIONAIS
+    #TRADICIONAIS
+
+ $idade = 18;
+ if ($idade >= 17)
+ {
+            echo("É maior de idade");
+ }
+
+  else
+ {
+            echo("é menor de idade<br>");
+ }
+
+ echo("<br> <br> <br>");
+
+
+
+#CONDICIONAL LINDA (Não fuinciona)
+$idade2 = 18;
+echo($idade2 >= 18?"é maior de idade":"menor de idade");
+
+
+# || OU
+# && E
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ variaveis.php
@@ -0,0 +1,25 @@
+<?php
+    #Variaveis são Varaveis por que varia 
+    $minhavariavel = "Outro Nome";
+    $numero = "1";
+    $decimal = "2.1";
+
+echo ("O nome da minha Variavel é: $minhavariavel<br>");
+echo ("O meu Número: $numero<br>");
+echo ("O meu Decimal é: $decimal<br>");
+
+#CONCATENAÇÃO DE VARIÁVEIS
+$sobrenome = "Michael";
+$nome = "Toniolo";
+
+
+echo("MEU NOME É: ". $nome . " E MEU SOBRENOME É ".$sobrenome);
+echo("<br>MEU NOME É $nome E MEU SOBRENOME É $sobrenome");
+
+
+
+
+
+
+
+?>
